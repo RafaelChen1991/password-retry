cb54989650b4e3a950b02bf4063e39433de63b96
Rafeal Chen <winduanckc@gmail.com>
2019-01-26 17:17:06 +0800

    password.1

---
+++ password.py
@@ -0,0 +1,12 @@
+password = str('a123456')
+
+x = 3
+while x > 0:
+	pw = input('請輸入密碼: (最多有3次機會)')
+	if pw != password:
+		x = x - 1
+		print('密碼錯誤! 還有', x, '次機會')
+	else:
+		print('登入成功')
+		break
+	
